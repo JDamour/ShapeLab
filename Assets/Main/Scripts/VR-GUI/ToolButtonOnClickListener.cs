e86794d14c4b0fba5d3e302b25099338d92cbb32
Sebastian Kasanzew <sebkasanzew@gmail.com>
2016-02-06 17:21:44 +0100

    Add onClickListener for Leap Toggle Button

---
+++ Assets/Main/Scripts/VR-GUI/ToolButtonOnClickListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolButtonOnClickListener : MonoBehaviour {
+
+    private ToolButtonToggle toolButtonToggle;
+
+    // Use this for initialization
+    void Start () {
+        toolButtonToggle = this.GetComponent<ToolButtonToggle>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (toolButtonToggle.ToggleState == true)
+        {
+            toolButtonToggle.ButtonTurnsOff();
+        }
+        else
+        {
+            toolButtonToggle.ButtonTurnsOn();
+        }
+    }
+}
