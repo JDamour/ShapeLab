66061568eb32433da4852e8991f116ad0a274295
Sebastian Kasanzew <sebkasanzew@gmail.com>
2016-04-15 13:47:05 +0200

    Replace MeteorDDP Plugin

---
+++ Assets/Main/Scripts/Webserver/MeteorConnection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+//using Net.DDP.Client;
+
+public class MeteorConnection : MonoBehaviour {
+    //private IDataSubscriber subscriber = new Subscriber();
+
+    // private DdpClient client;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log(System.Environment.Version);
+
+        ////subscriber is an instance of IDataSubscriber, which gets a callback
+        ////when a change comes in
+        //DDPClient client = new DDPClient(subscriber);
+
+        //// you can't use localhost in the simulator!
+        //client.Connect("192.168.0.1:3000");
+        //client.Subscribe("allMovies");
+
+        //client.Call("helloWorld");
+
+        //Debug.Log(subscriber);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Main/Scripts/Webserver/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿////using Net.DDP.Client;
+//using System;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Subscriber : IDataSubscriber
+//{
+//    public void DataReceived(Dictionary<string, object> data)
+//    {
+//        /*
+//        try {
+//            if (data["type"].ToString() == "sub")
+//            {
+//                Debug.Log(data["prodCode"].ToString() + ": " + data["prodName"].ToString() + ": collection: " + data["collection)"].ToString());
+//            }
+//        }
+//        catch (Exception ex)
+//        {
+//            Debug.Log(ex);
+//            throw;
+//        }
+//        */
+//    }
+//}
