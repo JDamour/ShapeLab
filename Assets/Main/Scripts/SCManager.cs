639eb96326df6b21caac474b1330795a51d810fb
ellerch <elias.lerch@t-online.de>
2016-06-05 19:41:00 +0200

    fixed ResetScreenshots

--- Assets/Main/Scripts/SCManager.cs
+++ Assets/Main/Scripts/SCManager.cs
@@ -9,46 +9,44 @@ public class SCManager : MonoBehaviour
     private RenderTexture[] SCTextureList;
     private RenderTexture[] SCTextureStartList;
 
-    private int currentIndex = 0;
+    private int currentScreenIndex = 0;
     // Use this for initialization
     void Start()
     {
         SCTextureList = new RenderTexture[SCScreenList.Length];
         for (int i = 0; i < SCScreenList.Length; i++)
         {
-            SCTextureList[i] = new RenderTexture(SCCam.pixelWidth, SCCam.pixelHeight, 16);
-            SCTextureList[i].Create();
-            SCTextureList[i].isPowerOfTwo = true;
+            SCTextureList[i] = createRenderTexture(SCCam.pixelWidth, SCCam.pixelHeight);
             SCScreenList[i].material.mainTexture = defaultTex;
         }
-
-        /*
-    for (int i = 0; i< SCScreenList.Length; i++)
-    {
-        SCTextureList[i] = new RenderTexture(SCCam.pixelWidth, SCCam.pixelHeight, 16);
-        SCTextureList[i].Create();
-        SCTextureList[i].isPowerOfTwo = true;
-        SCCam.targetTexture = SCTextureList[i];
-        SCCam.Render();
-        SCScreenList[i].material.SetTexture("", SCTextureList[i]);
     }
-    SCTextureStartList = SCTextureList;
-    */
+
+    public RenderTexture createRenderTexture(int width, int height)
+    {
+        RenderTexture tmp = new RenderTexture(width, height, 16);
+        tmp.Create();
+        tmp.isPowerOfTwo = true;
+        return tmp;
     }
 
     public void TakeScreenShoot()
     {
-        currentIndex++;
-        if (currentIndex >= SCScreenList.Length)
-            currentIndex = 0;
-        SCCam.targetTexture = SCTextureList[currentIndex];
+        currentScreenIndex++;
+        if (currentScreenIndex >= SCScreenList.Length)
+            currentScreenIndex = 0;
+        SCCam.targetTexture = SCTextureList[currentScreenIndex];
         SCCam.Render();
-        SCScreenList[currentIndex].material.mainTexture = SCTextureList[currentIndex];
+        SCScreenList[currentScreenIndex].material.mainTexture = SCTextureList[currentScreenIndex];
     }
     public void ResetScreenshots()
     {
-        SCTextureList = SCTextureStartList;
-        currentIndex = 0;
+        // delete old texture list
+        SCTextureList = new RenderTexture[SCScreenList.Length];
+        for (int i = 0; i < SCScreenList.Length; i++)
+        {
+            SCTextureList[i] = createRenderTexture(SCCam.pixelWidth, SCCam.pixelHeight);
+        }
+        currentScreenIndex = 0;
         for (int i = 0; i < SCScreenList.Length; i++)
         {
             SCScreenList[i].material.SetTexture("", SCTextureList[i]);
@@ -57,6 +55,6 @@ public class SCManager : MonoBehaviour
     public void ResetLastScreenshot()
     {
         //SCScreenList[currentIndex].material.mainTexture = SCTextureStartList[currentIndex];
-        SCScreenList[currentIndex].material.mainTexture = defaultTex;
+        SCScreenList[currentScreenIndex].material.mainTexture = defaultTex;
     }
 }
